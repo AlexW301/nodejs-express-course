50a052c8a059e3cb37a77e62bb30a24c3f627fb1
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2023-01-20 08:55:34 -0500

    1/20/23

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-let {people} = require("./data");
+
+const people = require("./routes/people");
+const auth = require("./routes/auth");
 
 // Static assets
 app.use(express.static("./methods-public"));
@@ -9,42 +11,8 @@ app.use(express.urlencoded({extended: false}));
 // parse json
 app.use(express.json());
 
-app.get("/api/people", (req, res) => {
-  res.status(200).json({success: true, data: people});
-});
-
-app.post("/login", (req, res) => {
-  const {name} = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-
-  res.status(401).send("Please provide credentials");
-});
-
-app.put("/api/people/:id", (req, res) => {
-  const {id} = req.params;
-  const {name} = req.body;
-  console.log(id, name);
-  res.send('hello world')
-});
-
-app.post("/api/postman/people", (req, res) => {
-  const {name} = req.body;
-  if (!name) {
-    return res.status(400).json({success: false, msg: "Please provide name value"});
-  }
-  res.status(201).send({success: true, data: [...people, name]});
-});
-
-app.post("/api/people", (req, res) => {
-  const {name} = req.body;
-  if (!name) {
-    return res.status(400).json({success: false, msg: "Please provide name value"});
-  }
-  res.status(201).json({success: true, person: name});
-});
-
+app.use("/api/people", people);
+app.use("/login", auth);
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000...`);
 });

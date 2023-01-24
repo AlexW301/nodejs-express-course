61246a856ecf2c459457581c9b2eeeb2668a6854
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2023-01-24 09:11:27 -0500

    1/24/23

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 // middleware
+
 app.use(express.json());
 
 // routes
@@ -14,4 +16,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start()
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.send("all items");
-});
+const {getAllTasks, createTask, updateTask, getTask, deleteTask} = require("../controllers/tasks");
+
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
